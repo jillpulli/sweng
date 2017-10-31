32249a6bc298c665c455dc1c9f5441ff69bc51f2
Jill Pulicicchio <jillianpulicicchio@gmail.com>
2017-10-31 12:57:44 -0400

    Add files via upload

---
+++ java/src/agile/MySQLtester (1).java
@@ -0,0 +1,29 @@
+package local;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+
+
+public class MySQLtester {
+
+	public static void main(String[] args) {
+		DataSource ds = ConnectionManager.getMySQLDataSource();
+		ResultSet rs = null;
+		
+		try (Connection con = ds.getConnection();
+			 Statement stmt = con.createStatement(); ) {
+			rs = stmt.executeQuery("select word from words where word like '%jack%'");
+			while(rs.next()){
+				System.out.println(rs.getString("word"));
+			}
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		} 
+	}
+}
+
