9259da2967be10da68453b1cae5ca884d937f36e
Jill Pulicicchio <jillianpulicicchio@gmail.com>
2017-10-31 12:55:44 -0400

    Add db files

---
+++ java/src/agile/ConnectionManager.java
@@ -0,0 +1,16 @@
+package local;
+
+
+import javax.sql.DataSource;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+public class ConnectionManager {
+	
+    public static DataSource getMySQLDataSource() {
+        MysqlDataSource mysqlDS = new MysqlDataSource();
+            mysqlDS.setServerName("127.0.0.1");
+            mysqlDS.setUser("myersjac");
+            mysqlDS.setPassword("p@ssw0rd");
+        return mysqlDS;
+    }
+}
