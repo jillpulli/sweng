14baad9135fab1dffc00955d649560ccf8a83205
StephCanada06 <canadas6@students.rowan.edu>
2017-11-20 20:06:57 -0500

    These are basic objects needed. Program Feature needs work Team Features and Product Features are done

--- java/src/agile/model/ProgramFeature.java
+++ java/src/agile/model/ProgramFeature.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 
 public class ProgramFeature extends Feature
 {
+    // Attributes
+
     String summary;
     int priorityScore;
     String CSLProgram;
@@ -14,16 +16,18 @@ public class ProgramFeature extends Feature
     /*
     ** This is Program Features Constructor
     */
-    public ProgramFeature(int s, String k, String d, String fv, String summary1, int pS, String cslp, int tS, Map<String, ProductFeature> dept)
+    public ProgramFeature( String k, String d, String fv, String summary1, int pS, String cslp, int tS, Map<String, ProductFeature> dept)
     {
-        super(s,k,d,fv);
+        super(k,d,fv);
         summary = summary1;
         priorityScore = pS;
         CSLProgram = cslp;
         totalSize = tS;
         departments = dept;
+        // Insert size must be implemented here!!
     }
 
+    // Accessor Methods
     public String getSummary()
     {
         return summary;
@@ -52,5 +56,21 @@ public class ProgramFeature extends Feature
     public void printFeature()
     {
         System.out.println("This print will be specific to this type of a feature!!");
+    }
+
+    // Modifier Methods
+
+    public void calcSize()
+    {
+        
+    }
+
+    /*
+    * The sum of all of the productFts without children and the in capacity team features of productFts with team features
+    * divided by the size of the program feature
+    */
+    public void calculatePercentInCapacity()
+    {
+
     }
 }
