86d51d305980a44ea580dcb6e9b2b8d6302df3cd
StephCanada06 <canadas6@students.rowan.edu>
2017-11-28 17:51:01 -0500

    Feature Factory has its giant method broken down into small methods which are called in the big method.

--- src/main/java/agile/Assembly/FeatureFactory.java
+++ src/main/java/agile/Assembly/FeatureFactory.java
@@ -18,40 +18,72 @@ public class FeatureFactory
     { // Instead of being void you will return ftAggregator
         FeatureAggregator featureTree = new FeatureAggregator();
         HashMap<String, String> currHM;
-        ProgramFeature currProgFT = new ProgramFeature("PLACEHOLDER", "WILL NEVER USE BC 1st CSV ENTRY IS PROGRAM FT", 0);
-        ProductFeature currProdFT = new ProductFeature("FILLERBUSTER", 0, false);
+        ProgramFeature currProgFT = null;
+        ProductFeature currProdFT = null;
+        TeamFeature currTeamFT = null;
         String currCSLProgram = " ";
 
-        for(int i = 0; i < database.size(); i++) // Gets the HashMap inside each index
+        for (int i = 0; i < database.size(); i++) // Gets the HashMap inside each index
         {
             currHM = database.get(i);
-            if(currHM.get("CSL Programs").equals(currCSLProgram))
-            { // This means a program feature has been instaniated
-                if (currHM.get("Level").equals(1))
-                {
-                    currProdFT = new ProductFeature(currHM.get("key"), Double.parseDouble(currHM.get("currentSize")), Boolean.valueOf(currHM.get("inCapacity")));
-                    currProgFT.addFeature(currHM.get("Project"), currProdFT);
-                    departmentSet.add(currHM.get("Project")); // Populates the departmentSet
-                }
-                else
-                {
-                    TeamFeature ft = new TeamFeature(currHM.get("key"), Double.parseDouble(currHM.get("currentSize")), Boolean.valueOf(currHM.get("inCapacity")));
-                    currProdFT.addFeature(ft);
-                }
-            }
-            else // If its a new CSL, add the previous Program Feature to the correct CSL
+            if (currHM.containsKey("CSL Programs"))
             {
-                if(!currCSLProgram.equals(" ")) // Makes sure nothing is added in the zeroth iteration since product feature has yet to be built
-                {
-                    featureTree.addFeature(currCSLProgram, currProgFT);
-                }
+                if (currHM.get("CSL Programs").equals(currCSLProgram))
+                { // This means a program feature has been instaniated
+                    if (currHM.get("Level").equals(1))
+                    {
+                        createProdFT(currHM, currProdFT);
+                        currProgFT.addFeature(currHM.get("Project"), currProdFT);
 
-                // Makes the new Program Feature
-                currProgFT = new ProgramFeature(currHM.get("key"), currHM.get("summary"), Integer.parseInt(currHM.get("priorityScore")));
+                        // Populates the departmentSet
+                        departmentSet.add(currHM.get("Project"));
+                    }
+                    else if (currHM.get("Level").equals(2))
+                    {
+                        createTeamFT(currHM, currTeamFT);
+                        currProdFT.addFeature(currTeamFT);
+                    }
+                    else
+                    {
+                        // If it is some other number it is an Feature Object outside of current existence
+                    }
+                } else // If its a new CSL, add the previous Program Feature to the correct CSL
+                {
+                    if (currCSLProgram != null) // Makes sure nothing is added in the zeroth iteration since product feature has yet to be built
+                    {
+                        featureTree.addFeature(currCSLProgram, currProgFT);
+                    }
+
+                    // Makes the new Program Feature
+                    createProgFT(currHM, currProgFT);
+                }
             }
         }
 
         return featureTree;
     }
 
+    private void createProgFT(HashMap<String, String> hm, ProgramFeature progFT)
+    {
+        if(hm.containsKey("key") && hm.containsKey("summary") && hm.containsKey("priorityscore"))
+        {
+            progFT = new ProgramFeature(hm.get("key"), hm.get("summary"), Integer.parseInt(hm.get("priorityScore")));
+        }
+    }
+
+    private void createProdFT(HashMap<String,String> hm, ProductFeature prodFT)
+    {
+        if(hm.containsKey("key") && hm.containsKey("currentSize") && hm.containsKey("inCapacity"))
+        {
+            prodFT = new ProductFeature(hm.get("key"), Double.parseDouble(hm.get("currentSize")), Boolean.valueOf(hm.get("inCapacity")));
+        }
+    }
+
+    private void createTeamFT(HashMap<String,String> hm, TeamFeature teamFT)
+    {
+        if(hm.containsKey("key") && hm.containsKey("currentSize") && hm.containsKey("inCapacity"))
+        {
+            teamFT = new TeamFeature(hm.get("key"), Double.parseDouble(hm.get("currentSize")), Boolean.valueOf(hm.get("inCapacity")));
+        }
+    }
 }
