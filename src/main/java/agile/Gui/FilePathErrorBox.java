b19c08251f69e40476fcf2270fb95e4d14d08b3e
cjgra <gralac4@students.rowan.edu>
2017-12-11 21:29:43 -0500

    trying to connect GUI to main code

---
+++ src/main/java/agile/Gui/FilePathErrorBox.java
@@ -0,0 +1,104 @@
+package agile.Gui;
+
+import javafx.scene.Scene;
+import javafx.scene.text.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+import javafx.scene.image.*;
+
+/*
+ * Bug: Want to upload a new file after importing a file
+ * ..
+ * RU SURE PAGE ALL PREVIOUS MATRICES WILL BE FORGOTTEN UNLESS EXPORTED
+ * NEEDS TO REST EVERYTHING
+ */
+
+
+public class FilePathErrorBox
+{
+    public static void display()
+    {
+        Button okayButton = new Button("close");
+        okayButton.setStyle("-fx-padding: 10 30 10 30;");
+        okayButton.setTooltip(new Tooltip(".."));
+
+        Image HoneyBadgerWaiting = new Image("src/main/HoneyBadgerWaves.png", 130, 130, false, false);
+        Label picture = new Label();
+        picture.setGraphic(new ImageView(HoneyBadgerWaiting));
+
+        /*
+         * This is Top
+         */
+        HBox top = new HBox();
+        top.setPadding(new Insets(15, 10, 15, 10));
+        top.setSpacing(15);
+        top.setStyle("-fx-background-color:  #154c83;");
+
+        /*
+        * This is the center
+        */
+        VBox leftCenter = new VBox();
+        leftCenter.setSpacing(18);
+        leftCenter.getChildren().add(picture);
+
+        VBox centerCenter = new VBox();
+        centerCenter.setSpacing(18);
+        Text msg = new Text("Error: No CSV has been uploaded.");
+        msg.setFont(Font.font("Times New Roman", 14));
+        centerCenter.setStyle("-fx-background-color:  #f5f5f5;");
+        centerCenter.getChildren().add(msg);
+        Text msg2 = new Text("Honey Badger Cassandra is confused.");
+        msg2.setFont(Font.font("Times New Roman", 14));
+        centerCenter.setStyle("-fx-background-color:  #f5f5f5;");
+        centerCenter.getChildren().add(msg2);
+        centerCenter.getChildren().add(okayButton);
+        centerCenter.setAlignment(Pos.CENTER);
+
+        VBox rightCenter = new VBox();
+        rightCenter.setPadding(new Insets(10, 10, 10, 10));
+        rightCenter.setSpacing(18);
+        rightCenter.getChildren().add(picture);
+
+        BorderPane center = new BorderPane();
+        center.setRight(rightCenter);
+        center.setLeft(leftCenter);
+        center.setCenter(centerCenter);
+
+
+        /*
+         * This is bottom
+         */
+        HBox bottom = new HBox();
+        bottom.setPadding(new Insets(10, 10, 10, 10));
+        bottom.setSpacing(15);
+        bottom.setStyle("-fx-background-color:  #f5f5f5");
+
+        /*
+        * This is the panel for the main window
+        */
+        BorderPane border = new BorderPane();
+        border.setTop(top);
+        border.setCenter(center);
+        border.setBottom(bottom);
+
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setMinWidth(300);
+        window.setMinHeight(200);
+
+        Scene scene = new Scene(border, 400, 200);
+        window.setScene(scene);
+        window.showAndWait();
+
+        /*
+         * Give the buttons functionality
+         */
+        okayButton.setOnAction( e -> window.close());
+    }
+}
---
+++ src/main/java/agile/Gui/ImportBox.java
@@ -0,0 +1,109 @@
+package agile.Gui;
+
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.*;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
+import java.io.File;
+import java.nio.file.*;
+import java.io.File.*;
+
+public class ImportBox
+{
+    public static Label display (String title, String message) {
+
+        String defaultPath = "C://Stephanie/Desktop/";
+
+        final Label filePath = new Label();
+        Stage window = new Stage();
+
+        TextField CSVPath = new TextField();
+        CSVPath.setPrefWidth(100);
+        CSVPath.setPromptText("CSV Import Path Here.");
+
+        /*
+         * Creates the primary stage which is referenced as Window
+         */
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(300);
+        window.setMinHeight(200);
+
+        /*
+         * Creates the buttons and other features
+         */
+        Label label = new Label();
+        label.setText(message);
+
+        Button importButton = new Button("Import");
+        importButton.setStyle("-fx-padding: 10 30 10 30;");
+        importButton.setTooltip(new Tooltip("Retrieves the CSV located from the File Path provided."));
+
+        Button cancelButton = new Button("Cancel");
+        cancelButton.setStyle("-fx-padding: 10 30 10 30;");
+        cancelButton.setTooltip(new Tooltip("Exits out of the window."));
+
+        /*
+         * Gives Buttons Functionality
+         */
+        importButton.setOnAction( e ->
+        {
+            String s;
+            System.out.println(CSVPath.getText());
+            if((CSVPath.getText() != null && !CSVPath.getText().isEmpty()))
+            {
+                filePath.setText(CSVPath.getText() + " is the path we are importing.");
+                //s = CSVPath.getText();
+                FilePathErrorBox.display();
+            }
+            else
+            {
+                filePath.setText("Invalid path. Setting to Default: " + defaultPath);
+                CSVPath.setText(defaultPath);
+            }
+
+
+            window.close();
+        }) ;
+
+        cancelButton.setOnAction( e -> window.close());
+
+        VBox infoBox = new VBox();
+        infoBox.setSpacing(18);
+        infoBox.getChildren().addAll(label, CSVPath, importButton, cancelButton,filePath);
+        infoBox.setAlignment(Pos.CENTER);
+
+        /*
+         * Creates top box with message
+         */
+        HBox top = new HBox();
+        top.setPadding(new Insets(15, 10, 15, 10));
+        top.setSpacing(15);
+        top.setStyle("-fx-background-color:  #154c83;");
+        Text header = new Text(message);
+        header.setTextAlignment(TextAlignment.CENTER);
+        header.setStyle("-fx-text-fill: #f5f5f5;");
+        header.setFont(Font.font("Times New Roman", FontWeight.BOLD, 18));
+        top.getChildren().add(header);
+
+        /*
+         * Main GUI's design
+         */
+        BorderPane mainImportGUI = new BorderPane();
+        mainImportGUI.setTop(top);
+        mainImportGUI.setCenter(infoBox);
+
+        Scene scene = new Scene(mainImportGUI, 500, 250);
+        window.setScene(scene);
+        window.showAndWait();
+
+        return filePath;
+
+    }
+}
